04b05a251ace7c4639cfebcbcb7ffb6da85f50b4
Joe Walnes <jwalnes@drwholdings.com>
2011-10-11 18:33:15 -0500

    Adding some docs

--- shiftbrite.h
+++ shiftbrite.h
@@ -18,18 +18,52 @@ typedef enum {
   CLOCK_200MHZ
 } shiftbrite_clock;
 
-static const uint8_t MAX_CORRECTION = 127;
-static const uint16_t MAX_COLOR_VALUE = 1023;
-
+/**
+ * Should be called at startup, after the pins have been set
+ * on the shiftbrite struct. This will configure the pins
+ * on the controller.
+ */
 void shiftbrite_init(shiftbrite *sb);
 
+/**
+ * Optional: configure color correction codes and clock speed.
+ *
+ * Correction codes allow you to tweak the output current of
+ * the LEDs - useful as a global brightness control and also to
+ * correct white balance caused by the LEDs not all being the
+ * same brightness.
+ *
+ * The correction values should be in the range 0 (dimmest) to
+ * 127 (brightest). Refere to the 'Dot Correction' section of
+ * the Allegro A6281 datasheet for details.
+ * http://www.allegromicro.com/en/Products/Part_Numbers/6281/6281.pdf
+ *
+ * The clock speed is also defined in the datasheet.
+ *
+ * When the system is powered up, the default correction values
+ * are all 127, and the clock is CLOCK_800MHZ.
+ *
+ * After calling shiftbrite_configure(), call shiftbrite_commit().
+ *
+ * If N modules are wired in a chain, shiftbrite_configure() should
+ * be called N times, followed by 1 call to shiftbrite_commit().
+ */
 void shiftbrite_configure(shiftbrite *sb,
                           uint8_t red_correction,
                           uint8_t green_correction,
                           uint8_t blue_correction,
                           shiftbrite_clock clock);
 
+/**
+ * Enable LED module. Enabling/disabling is a useful way to
+ * turn it on or off without having to reload the RGB or configuration
+ * values.
+ */
 void shiftbrite_enable(shiftbrite *sb);
+
+/**
+ * See shiftbrite_disable().
+ */
 void shiftbrite_disable(shiftbrite *sb);
 
 void shiftbrite_rgb(shiftbrite* sb,
